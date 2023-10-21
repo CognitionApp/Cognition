d7cf66246708cd6de7a039e3a4aa01e30b05f0ad
Tyler Shelton <tdshelton@gmail.com>
2023-10-21 09:15:52 -0700

    Initial webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_DEV,
+  // This property defines where the application starts
+  entry: './client/App.jsx',
+
+  // Define the filepath and file name used for deploying
+  // the bundled file
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+
+  // Setup loaders
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '...'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html'
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    hot: true,
+    proxy: { '/': 'http://localhost:3000' },
+    static: {
+      directory: path.join(__dirname, 'build'),
+    },
+  },
+  devtool: 'eval-source-map',
+};
