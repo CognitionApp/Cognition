6e826842dcaaca6bdaf1a318e046b7b9c76d045c
Tyler Shelton <tdshelton@gmail.com>
2023-10-23 11:50:15 -0700

    Define components for sidebar

---
+++ client/components/createMode/Sidebar.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+import SidebarListItem from "./SidebarListItem";
+
+const CreateModeSidebar = () => {
+  const [cardList, setCardList] = useState([]);
+
+  useEffect(() => {
+    async function getAllCards () {
+      const response = await fetch('/cards');
+      const cards = await response.json();
+      setCardList(cards);
+    }
+    getAllCards();
+  }, [])
+
+  const sidebarListItems = cardList.map((card, i) => {
+    return(<SidebarListItem
+      key = {`sidebarListItem-${card.id}`}
+      question = {card.question}
+      answer = {card.answer}
+    />);
+  })
+
+  return (
+    <nav className="sidebar">
+      <ul>
+        {sidebarListItems}
+      </ul>
+    </nav>
+  );
+};
+
+export default CreateModeSidebar;
