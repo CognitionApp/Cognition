04846bd85d3738dcfca781d4b0058dc1370c989a
Edward Li <edwardli744@gmail.com>
2023-10-23 15:08:23 -0700

    Sidebar now renders

--- client/components/createMode/Sidebar.jsx
+++ client/components/createMode/Sidebar.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
-import SidebarListItem from "./SidebarListItem";
+import SidebarListItem from './SidebarListItem';
 
-const CreateModeSidebar = () => {
+const CreateModeSidebar = ({ triggerRefresh }) => {
   const [cardList, setCardList] = useState([]);
 
   useEffect(() => {
-    async function getAllCards () {
+    async function getAllCards() {
       const response = await fetch('/cards');
       const cards = await response.json();
       setCardList(cards);
     }
     getAllCards();
-  }, [])
+  }, [triggerRefresh]);
 
   const sidebarListItems = cardList.map((card, i) => {
-    return(<SidebarListItem
-      key = {`sidebarListItem-${i}`}
-      id = {card.id}
-      question = {card.question}
-      answer = {card.answer}
-    />);
-  })
+    return (
+      <SidebarListItem
+        key={`sidebarListItem-${i}`}
+        id={card.id}
+        question={card.question}
+        answer={card.answer}
+      />
+    );
+  });
 
   return (
-    <nav className="sidebar">
-      <ul>
-        {sidebarListItems}
-      </ul>
+    <nav className='sidebar'>
+      <ul>{sidebarListItems}</ul>
     </nav>
   );
 };
