8e2af1649b56c7940e0fc3cc4a8a6255e3b0f209
Edward Li <edwardli744@gmail.com>
2023-10-23 12:58:12 -0700

    Was originally named wrong

--- client/components/createComponent.jsx
+++ client/components/createComponent.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import '../stylesheets/style.scss';
 
-
 const Create = (props) => {
-
-    return (
-      <div className= "flashContainer">
-        <div className='flashcard'> Flash card with flip functionality</div>
-      </div>  
-    )
-}
-
-
-
-
+  return (
+    <div className='flashContainer'>
+      <div className='flashcard'>
+        <label>Front: </label>
+        <input type='text' placeholder='Enter text here' />
+      </div>
+      <div className='flashcard'>
+        <label>Back: </label>
+        <input type='text' placeholder='Enter text here' />
+      </div>
+      <button className='button'>Submit</button>
+      <div className='container'>Flashcard Container</div>
+    </div>
+  );
+};
 
 export default Create;
