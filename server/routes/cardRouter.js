1a00f025e3dec4dbe1f480f4aa2084c0310c2103
Edward Li <edwardli744@gmail.com>
2023-10-23 08:35:47 -0700

    Changed naming conventions to match

--- server/routes/cardRouter.js
+++ server/routes/cardRouter.js
@@ -19,12 +19,12 @@ router.post('/', cardController.createCard, (req, res) => {
 
 // update an existing card
 router.patch('/:id', cardController.updateCard, (req, res) => {
-  return res.status(200).json(res.locals.result);
+  return res.status(200).json(res.locals.flashCard);
 });
 
 // delete an existing card
 router.delete('/:id', cardController.deleteCard, (req, res) => {
-  return res.status(200).json(`Deleted ${res.locals.flashCard}`);
+  return res.status(200).json(res.locals.flashCard);
 });
 
 module.exports = router;
